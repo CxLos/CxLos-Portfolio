c8aa100cc879fe55ac45011bb8550edea0f1f4c9
cxlos <bautistac_29@outlook.com>
2023-05-23 14:58:36 -0500

    added link to resume that launches you to website

--- cxlos20-portfolio/src/components/pages/resume.js
+++ cxlos20-portfolio/src/components/pages/resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = () => {
     return(
         <div className=''>
-
+            
             <div classname=''>
                 <header className=''>
                     <h1>Resume</h1>
@@ -15,7 +15,9 @@ const Resume = () => {
                         <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-224148.png`)}/>
                     </div> */}
                     <div className='rbox1'>
+                    <a href="https://docs.google.com/document/d/1rD4-PZOt2kMmxeYXZWkA_-xmUc_kqJ9EUQkMeJQnB58/">
                         <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-122247.png`)}/>
+                        </a>
                     </div>
                     <div className='rbox1'>
                         {/* <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-225613.png`)}/> */}
