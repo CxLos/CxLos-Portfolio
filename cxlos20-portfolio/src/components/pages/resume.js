580737432e4511d744bebc14cb0d84f1a9a0e515
cxlos <bautistac_29@outlook.com>
2023-05-23 16:49:21 -0500

    added pdf download feature to resume

--- cxlos20-portfolio/src/components/pages/resume.js
+++ cxlos20-portfolio/src/components/pages/resume.js
@@ -11,18 +11,12 @@ const Resume = () => {
             </div>
 
             <div className="rbox">
-                    {/* <div className='rbox1'>
-                        <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-224148.png`)}/>
-                    </div> */}
-                    <div className='rbox1'>
-                    <a href="https://docs.google.com/document/d/1rD4-PZOt2kMmxeYXZWkA_-xmUc_kqJ9EUQkMeJQnB58/">
-                        <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-122247.png`)}/>
-                        </a>
-                    </div>
-                    <div className='rbox1'>
-                        {/* <img className='resume' alt='resume' src={require(`./screenshots/Screenshot-225613.png`)}/> */}
-                    </div>
+                <div className='rbox1'>
+                    <a href="/Bautistac_resume.pdf" download="Bautista_resume.pdf">
+                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/Screenshot-122247.png`)}/>
+                    </a>
                 </div>
+            </div>
         </div>
     );
 };
