982a36d826015c02401e93aa42672c41b867cb6b
cxlos <bautistac_29@outlook.com>
2023-10-16 11:47:55 -0500

    updated resume

--- cxlos20-portfolio/src/components/pages/resume.js
+++ cxlos20-portfolio/src/components/pages/resume.js
@@ -15,7 +15,7 @@ const Resume = () => {
                     {/* <a href="https://docs.google.com/document/d/1rD4-PZOt2kMmxeYXZWkA_-xmUc_kqJ9EUQkMeJQnB58/edit" download="Bautista_resume.pdf"> */}
                     {/* <a href="CxLos20-React-Portfolio/Bautistac_resume.pdf" download="Bautista_resume.pdf"> */}
                     {/* <a href="/Bautistac_resume.pdf" download="Bautistac_resume.pdf"> */}
-                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/Screenshot-161317.png`)}/>
+                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/Screenshot-114105.png`)}/>
                     {/* </a> */}
                 </div>
                 <div className='rbox1'>
