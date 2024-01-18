c409f0b5eef5cd9057946f407f696313df041b07
cxlos <bautistac_29@outlook.com>
2024-01-18 17:49:46 -0600

    added Data Analysis with Python badge to page

--- cxlos20-portfolio/src/components/pages/badge.js
+++ cxlos20-portfolio/src/components/pages/badge.js
@@ -6,30 +6,59 @@ const Badge = () => {
         <div className="page">
           <header>
                 <h1>Badges</h1>
-            </header>
+          </header>
 
-          <div className='badge'>
-            <a href='https://www.credly.com/badges/61842971-ed83-498b-bf71-e0af10ad4648/public_url'>
+          <div className='badge-box'>
 
-              <div className='badge-title'>
-                <header className='badge-header'>
-                      <h1>Data Analysis with Python</h1>
-                </header>
-              </div>
-              
-              <div className='badge-logo'>
-                <img className='ibm' alt='Data Analysis with Python IBM Badge' src={require(`./screenshots/IBM1.png`)}/>
-              </div>
+            <div className='badge'>
+              <a href='https://www.credly.com/badges/61842971-ed83-498b-bf71-e0af10ad4648/public_url'>
 
-            </a>
+                <div className='badge-title'>
+                  <header className='badge-header'>
+                        <h1>Data Analysis with Python</h1>
+                  </header>
+                </div>
+                
+                <div className='badge-logo'>
+                  <img className='ibm' alt='Data Analysis with Python IBM Badge' src={require(`./screenshots/IBM1.png`)}/>
+                </div>
+
+              </a>
+            </div>
+
+            {/* <div className='badge'>
+              <a href='https://www.credly.com/badges/61842971-ed83-498b-bf71-e0af10ad4648/public_url'>
+
+                <div className='badge-title'>
+                  <header className='badge-header'>
+                        <h1>Data Visualization with Python</h1>
+                  </header>
+                </div>
+                
+                <div className='badge-logo'>
+                  <img className='ibm' alt='Data Analysis with Python IBM Badge' src={require(`./screenshots/IBM1.png`)}/>
+                </div>
+
+              </a>
+            </div>
+
+            <div className='badge'>
+              <a href='https://www.credly.com/badges/61842971-ed83-498b-bf71-e0af10ad4648/public_url'>
+
+                <div className='badge-title'>
+                  <header className='badge-header'>
+                        <h1>Data Visualization with Python</h1>
+                  </header>
+                </div>
+                
+                <div className='badge-logo'>
+                  <img className='ibm' alt='Data Analysis with Python IBM Badge' src={require(`./screenshots/IBM1.png`)}/>
+                </div>
+
+              </a>
+            </div> */}
+
           </div>
-          
-            
-
-           
-
-            
-
            {/* <div>
             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="61842971-ed83-498b-bf71-e0af10ad4648" data-share-badge-host="https://www.credly.com">
             </div>
