9502c4ca4b0c6f9f18f120e2ed89bfdeb343338c
cxlos <bautistac_29@outlook.com>
2024-01-18 18:19:15 -0600

    Made resume downloadable with a link

--- cxlos20-portfolio/src/components/pages/resume.js
+++ cxlos20-portfolio/src/components/pages/resume.js
@@ -12,19 +12,12 @@ const Resume = () => {
 
             <div className="rbox">
                 <div className='rbox1'>
-                    {/* <a href="https://docs.google.com/document/d/1rD4-PZOt2kMmxeYXZWkA_-xmUc_kqJ9EUQkMeJQnB58/edit" download="Bautista_resume.pdf"> */}
-                    {/* <a href="CxLos20-React-Portfolio/Bautistac_resume.pdf" download="Bautista_resume.pdf"> */}
-                    {/* <a href="/Bautistac_resume.pdf" download="Bautistac_resume.pdf"> */}
-                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/Screenshot-114105.png`)}/>
-                    {/* </a> */}
+                  
+                    <a href="https://onedrive.live.com/?cid=3585458A03CAD8DC&id=3585458a03cad8dc%2121847&parId=3585458a03cad8dc%218159&o=OneUp" download="Bautista_resume.pdf" className='resume-link'>
+                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/resume.png`)}/>
+                    </a> 
                 </div>
-                <div className='rbox1'>
-                    {/* <a href="https://docs.google.com/document/d/1rD4-PZOt2kMmxeYXZWkA_-xmUc_kqJ9EUQkMeJQnB58/edit" download="Bautista_resume.pdf"> */}
-                    {/* <a href="CxLos20-React-Portfolio/Bautistac_resume.pdf" download="Bautista_resume.pdf"> */}
-                    {/* <a href="/Bautistac_resume.pdf" download="Bautista_resume.pdf">
-                        <img className='resume' alt='resume' title='Download pdf' src={require(`./screenshots/Screenshot-154334.png`)}/>
-                    </a> */}
-                </div>
+
             </div>
         </div>
     );
