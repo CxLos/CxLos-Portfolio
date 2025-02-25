8744d95cad5367c16260b5a0d3cab37cef2fae49
CxLos <bautistac_29@outlook.com>
2025-02-24 22:23:13 -0600

    Updates

---
+++ precache-manifest.849e01c3dee54281b4e29eeddc2c600c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08ab8bc7c3407eef2fae1f6793cc6e3",
+    "url": "/CxLos-Portfolio/index.html"
+  },
+  {
+    "revision": "f7313165156e7b58508c",
+    "url": "/CxLos-Portfolio/static/css/main.dd61e4f8.chunk.css"
+  },
+  {
+    "revision": "14b7bd76b90102a86d39",
+    "url": "/CxLos-Portfolio/static/js/2.cc71e090.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/CxLos-Portfolio/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "f7313165156e7b58508c",
+    "url": "/CxLos-Portfolio/static/js/main.dce3f4f1.chunk.js"
+  },
+  {
+    "revision": "4605ab13f3d55c09a63e",
+    "url": "/CxLos-Portfolio/static/js/runtime~main.b3228ac6.js"
+  },
+  {
+    "revision": "cbac98f28cb24ed3c90bca7e7ac7c7a1",
+    "url": "/CxLos-Portfolio/static/media/IBM1.cbac98f2.png"
+  },
+  {
+    "revision": "23adb28bd0708c1dddceca0868030dd5",
+    "url": "/CxLos-Portfolio/static/media/IBM2.23adb28b.png"
+  },
+  {
+    "revision": "461eaf42719a7711cc4dcf56978fe152",
+    "url": "/CxLos-Portfolio/static/media/IBM3.461eaf42.png"
+  },
+  {
+    "revision": "247a97521a3bb68aa01a91479c9a07e5",
+    "url": "/CxLos-Portfolio/static/media/IBM4.247a9752.png"
+  },
+  {
+    "revision": "f98cd910425bf727bd54ce767a9b6884",
+    "url": "/CxLos-Portfolio/static/media/PressStart2P-Regular.f98cd910.ttf"
+  },
+  {
+    "revision": "b3ce22d5d6693adb93d0b1311f639e6d",
+    "url": "/CxLos-Portfolio/static/media/Screenshot-0828.b3ce22d5.png"
+  },
+  {
+    "revision": "2f277227614a394337d016ffe0ff5cd7",
+    "url": "/CxLos-Portfolio/static/media/Screenshot-1057.2f277227.png"
+  },
+  {
+    "revision": "4d45bb0eb85c805c5e70526c91cfe578",
+    "url": "/CxLos-Portfolio/static/media/Screenshot-151238.4d45bb0e.png"
+  },
+  {
+    "revision": "41a92897ef7bb170650d0c7bb8041857",
+    "url": "/CxLos-Portfolio/static/media/Screenshot-185839.41a92897.png"
+  },
+  {
+    "revision": "44f7dc5339e3c6e3431fe983d7abb6a4",
+    "url": "/CxLos-Portfolio/static/media/Screenshot-191817.44f7dc53.png"
+  },
+  {
+    "revision": "fd855e79847269c08e92258d61c60649",
+    "url": "/CxLos-Portfolio/static/media/headshot4.fd855e79.JPG"
+  },
+  {
+    "revision": "e81034e6082af360b80f286aff30513a",
+    "url": "/CxLos-Portfolio/static/media/pokemon-vector.e81034e6.jpg"
+  },
+  {
+    "revision": "b88f785bccdb9ed2c383124eb921f589",
+    "url": "/CxLos-Portfolio/static/media/resume.b88f785b.png"
+  }
+]);
